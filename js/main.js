f3f8287d2c0cb5ef87dabde481bcf2f355b66e23
AbaevB <abaev70@hotmail.com>
2023-09-05 11:14:39 +0700

    feat(layout): hero

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  autoplay: {
+    delay: 5000,
+  },
+  effect: 'flip',
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+});
